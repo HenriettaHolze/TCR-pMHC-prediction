092843d2370919b05e47e2acdc3b5f803c1d615e
Henrietta Holze <henrietta.holze@cpr.ku.dk>
2021-11-02 14:13:06 +0100

    add data and scripts from the hackathon github repo

---
+++ hackathon_data_scripts/src/predict.py
@@ -0,0 +1,85 @@
+import argparse
+import glob, os, tempfile, zipfile
+
+import pandas as pd
+import numpy as np
+from sklearn import metrics
+from sklearn.metrics import f1_score, accuracy_score
+from sklearn.metrics import roc_curve, confusion_matrix
+import torch
+import torch.nn as nn  # All neural network modules, nn.Linear, nn.Conv2d, BatchNorm, Loss functions
+import torch.optim as optim  # For all Optimization algorithms, SGD, Adam, etc.
+import torch.nn.functional as F  # All functions that don't have any parameters
+from sklearn.metrics import accuracy_score
+
+# Needed to predict
+from model import Net
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input-zip')
+args = parser.parse_args()
+print(args)
+
+# Load files
+filenames = []
+dfs = []
+with tempfile.TemporaryDirectory() as tmpdir:
+    with zipfile.ZipFile(args.input_zip) as zip:
+        files = [file for file in zip.infolist() if file.filename.endswith(".npz")]
+        for file in files:
+            zip.extract(file, tmpdir)
+            
+        # Load npz files
+        data_list = []
+
+        for fp in glob.glob(tmpdir + "/*input.npz"):
+            data = np.load(fp)["arr_0"]
+
+            data_list.append(data)
+         
+X_test = np.concatenate(data_list[:])
+nsamples, nx, ny = X_test.shape
+print("test set shape:", nsamples,nx,ny)
+
+test_ds = []
+for i in range(len(X_test)):
+    test_ds.append([np.transpose(X_test[i])])
+
+bat_size = 64
+print("\nNOTE:\nSetting batch-size to", bat_size)
+test_ldr = torch.utils.data.DataLoader(test_ds,batch_size=bat_size, shuffle=False)
+
+# Set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device (CPU/GPU):", device)
+#device = torch.device("cpu")
+
+def predict(net, test_ldr):
+    net.eval()
+    test_preds = []
+    with torch.no_grad():
+        for batch_idx, data in enumerate(test_ldr): ###
+            x_batch_val = data[0].float().detach()
+
+            output = net(x_batch_val)
+            preds = np.round(output.detach())
+            test_preds += list(preds.data.numpy().flatten()) 
+        
+    return(test_preds)
+
+    
+
+# import trained model
+model = Net(num_classes = 1)
+model.load_state_dict(torch.load("src/model.pt"))
+model.eval()
+
+y_pred = predict(model, test_ldr)
+
+# Write y_true, y_pred to disk
+outname = "predictions.csv"
+print("\nSaving TEST set y_pred to", outname)
+df_performance = pd.DataFrame({"ix": range(len(y_pred)), "prediction": y_pred},)
+df_performance.to_csv(outname, index=False)
+
+print(open(outname, 'r').read())
---
+++ hackathon_data_scripts/src/training.py
@@ -0,0 +1,165 @@
+import glob
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.metrics import f1_score, accuracy_score
+from sklearn.metrics import roc_curve, confusion_matrix
+import torch
+import torch.nn as nn  # All neural network modules, nn.Linear, nn.Conv2d, BatchNorm, Loss functions
+import torch.optim as optim  # For all Optimization algorithms, SGD, Adam, etc.
+import torch.nn.functional as F  # All functions that don't have any parameters
+from sklearn.metrics import accuracy_score
+
+###############################
+###    Load data            ###
+###############################
+
+data_list = []
+target_list = []
+
+for fp in glob.glob("data/train/*input.npz"):
+    data = np.load(fp)["arr_0"]
+    targets = np.load(fp.replace("input", "labels"))["arr_0"]
+    
+    data_list.append(data)
+    target_list.append(targets)
+
+# Note:
+# Choose your own training and val set based on data_list and target_list
+# Here using the last partition as val set
+
+X_train = np.concatenate(data_list[ :-1])
+y_train = np.concatenate(target_list[:-1])
+nsamples, nx, ny = X_train.shape
+print("Training set shape:", nsamples,nx,ny)
+
+X_val = np.concatenate(data_list[-1: ])
+y_val = np.concatenate(target_list[-1: ])
+nsamples, nx, ny = X_val.shape
+print("val set shape:", nsamples,nx,ny)
+
+p_neg = len(y_train[y_train == 1])/len(y_train)*100
+print("Percent positive samples in train:", p_neg)
+
+p_pos = len(y_val[y_val == 1])/len(y_val)*100
+print("Percent positive samples in val:", p_pos)
+
+# make the data set into one dataset that can go into dataloader
+train_ds = []
+for i in range(len(X_train)):
+    train_ds.append([np.transpose(X_train[i]), y_train[i]])
+
+val_ds = []
+for i in range(len(X_val)):
+    val_ds.append([np.transpose(X_val[i]), y_val[i]])
+
+bat_size = 64
+print("\nNOTE:\nSetting batch-size to", bat_size)
+train_ldr = torch.utils.data.DataLoader(train_ds,batch_size=bat_size, shuffle=True)
+val_ldr = torch.utils.data.DataLoader(val_ds,batch_size=bat_size, shuffle=True)
+
+
+# Set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device (CPU/GPU):", device)
+#device = torch.device("cpu")
+
+
+
+
+###############################
+###    Define network       ###
+###############################
+
+print("Initializing network")
+
+# Hyperparameters
+input_size = 420
+num_classes = 1
+learning_rate = 0.01
+
+# Initialize network
+from model import Net
+net = Net(num_classes=num_classes).to(device)
+
+# Loss and optimizer
+criterion = nn.BCELoss()
+optimizer = optim.SGD(net.parameters(), lr=learning_rate)
+
+
+
+###############################
+###         TRAIN           ###
+###############################
+
+print("Training")
+
+num_epochs = 5
+
+train_acc, train_loss = [], []
+valid_acc, valid_loss = [], []
+losses = []
+val_losses = []
+
+for epoch in range(num_epochs):
+    cur_loss = 0
+    val_loss = 0
+    
+    net.train()
+    train_preds, train_targs = [], [] 
+    for batch_idx, (data, target) in enumerate(train_ldr):
+        X_batch =  data.float().detach().requires_grad_(True)
+        target_batch = torch.tensor(np.array(target), dtype = torch.float).unsqueeze(1)
+        
+        optimizer.zero_grad()
+        output = net(X_batch)
+        
+        batch_loss = criterion(output, target_batch)
+        batch_loss.backward()
+        optimizer.step()
+        
+        preds = np.round(output.detach().cpu())
+        train_targs += list(np.array(target_batch.cpu()))
+        train_preds += list(preds.data.numpy().flatten())
+        cur_loss += batch_loss.detach()
+
+    losses.append(cur_loss / len(train_ldr.dataset))
+        
+    
+    net.eval()
+    ### Evaluate validation
+    val_preds, val_targs = [], []
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(val_ldr): ###
+            x_batch_val = data.float().detach()
+            y_batch_val = target.float().detach().unsqueeze(1)
+            
+            output = net(x_batch_val)
+            
+            val_batch_loss = criterion(output, y_batch_val)
+            
+            preds = np.round(output.detach())
+            val_preds += list(preds.data.numpy().flatten()) 
+            val_targs += list(np.array(y_batch_val))
+            val_loss += val_batch_loss.detach()
+            
+        val_losses.append(val_loss / len(val_ldr.dataset))
+        print("\nEpoch:", epoch+1)
+        
+        train_acc_cur = accuracy_score(train_targs, train_preds)  
+        valid_acc_cur = accuracy_score(val_targs, val_preds) 
+
+        train_acc.append(train_acc_cur)
+        valid_acc.append(valid_acc_cur)
+        
+        from sklearn.metrics import matthews_corrcoef
+        print("Training loss:", losses[-1].item(), "Validation loss:", val_losses[-1].item(), end = "\n")
+        print("MCC Train:", matthews_corrcoef(train_targs, train_preds), "MCC val:", matthews_corrcoef(val_targs, val_preds))
+        
+print('\nFinished Training ...')
+
+# Write model to disk for use in predict.py
+print("Saving model to src/model.pt")
+torch.save(net.state_dict(), "src/model.pt")
